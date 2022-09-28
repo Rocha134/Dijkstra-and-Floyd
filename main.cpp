f0f9aec5cc9b5d1a1584ed5654ad76d2a9763175
Rocha134 <rochapakito@gmail.com>
2022-09-28 12:47:13 -0500

    Floyd vector done, printing is missing

--- main.cpp
+++ main.cpp
@@ -113,7 +113,6 @@ vector<int> dijkstraDist(
     return dist;
 }
 
-
 // Driver Code
 int main()
 {
@@ -123,7 +122,7 @@ int main()
 
     int n;
     cin >> n;
-    int arr[n][n], i, j;
+    int arr[n][n], i, j,k;
 
     // Loop to create the nodes
     for (int i = 0; i < n; i++) {
@@ -180,6 +179,50 @@ int main()
     }
     while (s < n);
 
+    //Implementation of Floyd Algorithm
+
+    /* dist[][] will be the output matrix
+    that will finally have the shortest
+    distances between every pair of vertices */
+    int dist[n][n];
+
+    /* Initialize the solution matrix same
+    as input graph matrix. Or we can say
+    the initial values of shortest distances
+    are based on shortest paths considering
+    no intermediate vertex. */
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            dist[i][j] = arr[i][j];
+
+    /* Add all vertices one by one to
+    the set of intermediate vertices.
+    ---> Before start of an iteration,
+    we have shortest distances between all
+    pairs of vertices such that the
+    shortest distances consider only the
+    vertices in set {0, 1, 2, .. k-1} as
+    intermediate vertices.
+    ----> After the end of an iteration,
+    vertex no. k is added to the set of
+    intermediate vertices and the set becomes {0, 1, 2, ..
+    k} */
+    for (k = 0; k < n; k++) {
+        // Pick all vertices as source one by one
+        for (i = 0; i < n; i++) {
+            // Pick all vertices as destination for the
+            // above picked source
+            for (j = 0; j < n; j++) {
+                // If vertex k is on the shortest path from
+                // i to j, then update the value of
+                // dist[i][j]
+                if (dist[i][j] > (dist[i][k] + dist[k][j])
+                    && (dist[k][j] != infi
+                        && dist[i][k] != infi))
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
 
 
     return 0;
